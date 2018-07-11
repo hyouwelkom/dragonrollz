2c9f01957865bf9b6d26360547f19cc4de70d1c0
misuto83 <virgile.bessiere@gmail.com>
2018-07-12 01:59:57 +0200

    refixed bracket

--- index.js
+++ index.js
@@ -34,79 +34,83 @@ bot.on('message', function (msg) {
                 break
         }
 
-        if(msg.content.toLowerCase().includes("lucas ") || msg.content.toLowerCase().includes("lulu ") || msg.content.toLowerCase() === "lucas" || msg.content.toLowerCase() === "lulu" ) {
+        if (msg.content.toLowerCase().includes("lucas ") || msg.content.toLowerCase().includes("lulu ") || msg.content.toLowerCase() === "lucas" || msg.content.toLowerCase() === "lulu") {
             msg.channel.send("Dieu")
         }
-        if(msg.content.startsWith("dv type ")) {
+        if (msg.content.startsWith("dv type ")) {
             var str = msg.content
             msg.delete()
             str = str.replace('dv type ', '')
             msg.channel.send(str)
         }
-        if(msg.content.startsWith("dv help")) {
+        if (msg.content.startsWith("dv help")) {
             const embed = new Discord.RichEmbed().setColor(0x20B2AA)
             for (var cmd in commands) {
-                embed.addField('\n'+commands[cmd].name+'\n', '\n**Description:** '+commands[cmd].desc+'\n\n**Usage:** '+commands[cmd].usage+'\n\n')
+                embed.addField('\n' + commands[cmd].name + '\n', '\n**Description:** ' + commands[cmd].desc + '\n\n**Usage:** ' + commands[cmd].usage + '\n\n')
             }
             embed.setFooter("N'hésitez pas à me DM si vous avez une suggestion ou bien repéré un bug !")
             embed.setDescription('**Commandes du bot David Ghetto**\n')
             msg.channel.send(embed)
         }
-        if(msg.content.startsWith("dv cat")) {
+        if (msg.content.startsWith("dv cat")) {
             msg.channel.send("Actualités\nAfrique\nAlcool\nAmitié\nAmour\nArgent\nArmes\nAvenir\nBattle\nBonheur\nBusiness\nCinéma\nClash\nCouple\nDélinquance\nDieu\nDivers\nDrogue\nDrôle\nÉchec\nÉcole\nEgo Trip\nÉté\nExplicit Lyrics\nFamille\nFemme\nFilm\nFoot\nFrance\nFreestyle\nGeek\nIntégration\nInternet\nJalousie\nJeu de mots\nJeunesse\nJeux Vidéo\nJustice\nLeçon de vie\nManga\nMédias\nMère\nMort\nNoël\nNuit\nOld School\nPauvreté\nPolice\nPolitique\nPresse\nPrison\nProverbe\nRacisme\nRap Game\nRap US\nRateau\nReligion\nRéussite\nRue\nRupture\nSanté\nSexe\nSociété\nSport\nTélévision\nTemps\nTerrorisme\nTolérance\nTravail\nTriste\nVie\nViolence\nVoiture\n")
         }
-        if(msg.content.toLowerCase() === "dv punch " || msg.content.toLowerCase() === "dv punch") {
-            var i = Math.floor(Math.random() * (punch.length-1))
+        if (msg.content.toLowerCase() === "dv punch " || msg.content.toLowerCase() === "dv punch") {
+            var i = Math.floor(Math.random() * (punch.length - 1))
             msg.channel.send(punch[i].content)
             LastPunch.lastId = i
         }
-        if(msg.content.startsWith("dv punch ") && arr.length>2 && arr[2] !== undefined) {
+        if (msg.content.startsWith("dv punch ") && arr.length > 2 && arr[2] !== undefined) {
             str = msg.content.replace('dv punch ', '')
-            if(str.charAt(str.length-1) == " ") { str = str.slice(0, -1); }
+            if (str.charAt(str.length - 1) == " ") {
+                str = str.slice(0, -1);
+            }
             var tab = []
             var t = punch.length
-            for (i = 0;i<t;i++) {
+            for (i = 0; i < t; i++) {
                 var size = punch[i].tags.length
-                for(var y = 0; y<size; y++) {
-                    if(punch[i].tags[y] == str) { tab.push(punch[i]) }
+                for (var y = 0; y < size; y++) {
+                    if (punch[i].tags[y] == str) {
+                        tab.push(punch[i])
+                    }
                 }
             }
-            i = Math.floor(Math.random() * (tab.length-1))
-            if(tab.length>0) {
+            i = Math.floor(Math.random() * (tab.length - 1))
+            if (tab.length > 0) {
                 msg.channel.send(tab[i].content)
                 LastPunch.lastId = i
             } else {
                 msg.channel.send("Sorry fréro ta catégorie a pas été trouvée, mdr msk comment elle doit être rincée")
             }
         }
-        if(msg.content.toLowerCase().includes("david auteur ") || msg.content.toLowerCase() === "david auteur") {
-            msg.channel.send("La punch est de "+punch[LastPunch.lastId].author+" du titre "+punch[LastPunch.lastId].title+", album "+punch[LastPunch.lastId].album)
+        if (msg.content.toLowerCase().includes("david auteur ") || msg.content.toLowerCase() === "david auteur") {
+            msg.channel.send("La punch est de " + punch[LastPunch.lastId].author + " du titre " + punch[LastPunch.lastId].title + ", album " + punch[LastPunch.lastId].album)
         }
-        if(msg.content.toLowerCase().includes("kaktus ") || msg.content.toLowerCase() === "kaktus") {
+        if (msg.content.toLowerCase().includes("kaktus ") || msg.content.toLowerCase() === "kaktus") {
             msg.channel.send("Lpb des rappeuses")
         }
-        if(msg.content.toLowerCase().includes("coco ") || msg.content.toLowerCase() === "coco") {
+        if (msg.content.toLowerCase().includes("coco ") || msg.content.toLowerCase() === "coco") {
             msg.channel.send("Lpb des rappeurs")
         }
-        if(msg.content.toLowerCase().includes("virgile ") || msg.content.toLowerCase().includes("vivi ") || msg.content.toLowerCase() === "virgile" || msg.content.toLowerCase() === "vivi") {
+        if (msg.content.toLowerCase().includes("virgile ") || msg.content.toLowerCase().includes("vivi ") || msg.content.toLowerCase() === "virgile" || msg.content.toLowerCase() === "vivi") {
             msg.channel.send("Mon humble maître, souverain et vénéré créateur de toute chose")
         }
-        if(msg.content.toLowerCase().includes("océ ") || msg.content.toLowerCase() === "océ") {
+        if (msg.content.toLowerCase().includes("océ ") || msg.content.toLowerCase() === "océ") {
             msg.channel.send("Lpb des Serpentard")
         }
-        if(msg.content.toLowerCase().includes("nejma ") || msg.content.toLowerCase() === "nejma") {
+        if (msg.content.toLowerCase().includes("nejma ") || msg.content.toLowerCase() === "nejma") {
             msg.channel.send("BoT des îles et lpb des amatrices de tiramisu")
         }
-        if(msg.content.toLowerCase().includes("claire ") || msg.content.toLowerCase() === "claire") {
+        if (msg.content.toLowerCase().includes("claire ") || msg.content.toLowerCase() === "claire") {
             msg.channel.send("L'enfant de Columbiiiine, est au pieds des montaaagnes :musical_note:")
         }
-        if(msg.content.toLowerCase().includes("julien ") || msg.content.toLowerCase() === "julien") {
+        if (msg.content.toLowerCase().includes("julien ") || msg.content.toLowerCase() === "julien") {
             msg.channel.send(":flag_ma:")
         }
-        if(msg.content.toLowerCase().includes("athem ") || msg.content.toLowerCase() === "athem") {
+        if (msg.content.toLowerCase().includes("athem ") || msg.content.toLowerCase() === "athem") {
             msg.channel.send("C'est le H devant fils de pute")
         }
-        if(msg.content.toLowerCase().includes("geo") || msg.content.toLowerCase().includes("géo") || msg.content.toLowerCase() === "geo" || msg.content.toLowerCase() === "géo") {
+        if (msg.content.toLowerCase().includes("geo") || msg.content.toLowerCase().includes("géo") || msg.content.toLowerCase() === "geo" || msg.content.toLowerCase() === "géo") {
             var i = Math.floor(Math.random() * 12)
             switch (i) {
                 case 0:
@@ -143,14 +147,14 @@ bot.on('message', function (msg) {
                     msg.channel.send("Geoxx la chaussure qui respire", {
                         files: [
                             "./assets/img/geo2.png"
-                            ]
+                        ]
                     })
                     break
                 case 11:
                     msg.channel.send("", {
                         files: [
                             "./assets/img/geo1.jpeg"
-                            ]
+                        ]
                     })
                     break
 
@@ -158,25 +162,24 @@ bot.on('message', function (msg) {
         }
 
 
-
-        if(msg.content.toLowerCase() === "vas-y charge le") {
+        if (msg.content.toLowerCase() === "vas-y charge le") {
             msg.channel.send("Rien qu'ça téfri...").then(function (msg) {
-                var interval = setTimeout(function() {
+                var interval = setTimeout(function () {
                     msg.edit("Rien qu'ça téfri.....")
                 }, 1 * 1000)
-                var interval = setTimeout(function() {
+                var interval = setTimeout(function () {
                     msg.edit("Rien qu'ça téfri..........")
                 }, 1 * 1000)
-                var interval = setTimeout(function() {
+                var interval = setTimeout(function () {
                     msg.edit("Rien qu'ça téfri toujours..............")
                 }, 1 * 1000)
-                var interval = setTimeout(function() {
+                var interval = setTimeout(function () {
                     msg.edit("```(____((____________()~~~```")
                 }, 1 * 1000)
             })
         }
 
-
+    }
 })
 
 bot.login('MzM0MDkzMDMxNzAxNjEwNTA2.DifLJQ.jGTvGhIBgI-nhb-SwoqjCihszak')
